8ba4d2487638d504cce1fc7c1ba345ddcea8063b
tnorber2 <tnorber2@ford.com>
2021-03-02 11:26:18 -0500

    opening email access so people can email back to server to get out of office reply.

--- security.tf
+++ security.tf
@@ -101,25 +101,25 @@ resource "aws_security_group" "email_server" {
         protocol = "tcp"
     }
     ingress {
-        cidr_blocks = var.access_cidrs
+        cidr_blocks = ["0.0.0.0/0"]
         from_port = 995
         to_port = 995
         protocol = "tcp"
     }
     ingress {
-        cidr_blocks = var.access_cidrs
+        cidr_blocks = ["0.0.0.0/0"]
         from_port = 993
         to_port = 993
         protocol = "tcp"
     }
     ingress {
-        cidr_blocks = var.access_cidrs
+        cidr_blocks = ["0.0.0.0/0"]
         from_port = 25
         to_port = 25
         protocol = "tcp"
     }
     ingress {
-        cidr_blocks = var.access_cidrs
+        cidr_blocks = ["0.0.0.0/0"]
         from_port = 465
         to_port = 465
         protocol = "tcp"
